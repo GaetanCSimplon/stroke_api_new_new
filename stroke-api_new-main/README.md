81af8af4c1b68f1b50e072eb1ee03609a33823cb
GaetanCSimplon <gcourtieupro@gmail.com>
2025-08-28 09:12:01 +0200

    Rédaction README

--- stroke-api_new-main/README.md
+++ stroke-api_new-main/README.md
@@ -3,4 +3,21 @@ Stroke data project
 
 Ce projet contient les fichiers nécessaires au brief Stroke data - Développement d'une API REST et visualisation.
 
+===================
+
+Démarrage de l'application
+
+- Démarrer l'API (local)
+poetry run fastapi dev main.py
+- Démarrer Streamlit (local)
+poetry run streamlit run streamlit_app/app.py
+
+=====================
+
+Etat d'avancement du projet
+
+- Création de l'API : ok
+- Création du dashboard : ok
+- Fonctionnalités intégrée : ok
+- Documentation : Site généré mais pas déployé
 
--- stroke-api_new-main/stroke_api/streamlit_app/pages/donnees.py
+++ stroke-api_new-main/stroke_api/streamlit_app/pages/donnees.py
@@ -4,7 +4,7 @@ import requests
 
 from utils.data import load_data
 
-API_URL = "http://127.0.0.1:8001/patients/"
+API_URL = "http://127.0.0.1:8000/patients/"
 
 st.header('Rechercher des patients')
 
@@ -15,7 +15,7 @@ gender = st.selectbox("Genre", options=['', 'Male', 'Female'])
 # Penser à modifier les paramètres de age_max > 100 !!!
 # age_max = st.slider('Âge max.', min_value=0, max_value=100, step=1)
 # --- Slider de sélection de tranche d'âge, modification des paramètres
-age_max = st.select_slider('Âge max.', options=list(range(0,100)), value=(30,50))
+age_max = st.select_slider('Âge', options=list(range(0,100)), value=(30,50))
 stroke = st.selectbox('Historique AVC', options=['', '0', '1'])
 
 # Paramètrage API
